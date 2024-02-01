1231c6c2e02b6b63acba6c4b2ae2186e28925ba7
Hrvoje Zec <hrvoje.zec99@gmail.com>
2024-02-01 17:50:14 +0100

    Fix scrolling background while menu is open

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ AOS.init();
 
 const toggleMenu = () => document.body.classList.toggle("open");
 
-var splide = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 3,
-    perMove: 1,
-    rewind : true,
-    padding:'6rem',
-    gap: '1.5rem',
-    pagination: false,
-    breakpoints: {
-        768:{
-            perPage: 2,
-            gap: '1.5rem',
-            padding:'3rem',
-        },
-        480:{
-            perPage: 1,
-            gap: '1.5rem',
-            padding:'2rem',
-        }
+var splide = new Splide(".splide", {
+  type: "loop",
+  perPage: 3,
+  perMove: 1,
+  rewind: true,
+  padding: "6rem",
+  gap: "1.5rem",
+  pagination: false,
+  breakpoints: {
+    768: {
+      perPage: 2,
+      gap: "1.5rem",
+      padding: "3rem",
     },
-  } );
-  
-  splide.mount();
+    480: {
+      perPage: 1,
+      gap: "1.5rem",
+      padding: "2rem",
+    },
+  },
+});
+
+splide.mount();
